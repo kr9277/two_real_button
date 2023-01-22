0bb857e70f6cc9a0b5ead87fdbd8ef30e0ed4d7a
kr9277 <kr9277@bs.amalnet.k12.il>
2023-01-22 11:30:07 +0200

    a button2

---
+++ app/src/main/java/com/example/two_real_button/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.two_real_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+int i = 0;
+Button button;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              i ++;
+              button.setText("This is a click number: " + i);
+
+            }
+        });
+
+    }
+}
